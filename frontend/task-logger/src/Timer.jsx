4579758e0776fb09ea77b6f2e2b45170ce0263f6
Jeffrey MacPherson <jeff_macpherson95@hotmail.com>
2024-07-20 22:20:44 -0300

    new timer function

--- frontend/task-logger/src/Timer.jsx
+++ frontend/task-logger/src/Timer.jsx
@@ -1,66 +1,49 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 function Timer() {
-    const [seconds, setSeconds] = useState(0);
-    const [mins, setMins] = useState(0);
-    const [hours, setHours] = useState(0);
-    const [isRunning, setIsRunning] = useState(false);
 
-    let timer;
+    const [time, setTime] = useState(0);
+    const [running, setRunning] = useState(false);
 
     useEffect(() => {
-        if (isRunning) {
-            timer = setInterval(() => {
-                setSeconds(prevSeconds => {
-                    if (prevSeconds === 59) {
-                        setSeconds(0);
-                        setMins(prevMins => {
-                            if (prevMins === 59) {
-                                setMins(0);
-                                setHours(prevHours => prevHours + 1);
-                            } else {
-                                setMins(prevMins + 1);
-                            }
-                        });
-                    } else {
-                        return prevSeconds + 1;
-                    }
-                });
+        return () => clearInterval(id.current);
+    }, []);
+
+    let id = useRef();
+    
+    function handleTime() {
+        if (!running) {
+            console.log("Running");
+            id.current = setInterval(() => {
+                setTime((prev) => prev+1);
             }, 1000);
+            setRunning(true);
+        } else {
+            console.log("couldnt start")
         }
-        return () => clearInterval(timer);
-    }, [isRunning]);
-
-    const startTimer = () => {
-        setIsRunning(true);
-        
     }
 
-    const stopTimer = () => {
-        setIsRunning(false);
-        clearInterval(timer);
-    }
-
-    const resetTimer = () => {
-        setIsRunning(false);
-        clearInterval(timer);
-        setSeconds(0);
-        setMins(0);
-        setHours(0);
-    }
 
     return (
         <>
-            <div className="timer-container">
-                <div className="timer"><span className="numbers">{hours}:{mins < 10 ? '0' + mins : mins}:{seconds < 10 ? '0' + seconds : seconds}</span></div>
-                <div className="timer-btns">
-                    <button className="start" onClick={startTimer} disabled={isRunning}>Start</button>
-                    <button className="stop" onClick={stopTimer}>Stop</button>
-                    <button className="reset" onClick={resetTimer}>Reset</button>
-                </div>
-            </div>
+        <div className="timer">
+            <h2>{time}</h2>
+            <button onClick={()=> {
+                handleTime();
+            }}>Start</button>
+            <button onClick={()=> {
+                clearInterval(id.current);
+                setRunning(false);
+            }}>Pause</button>
+            <button onClick={()=> {
+                clearInterval(id.current);
+                setTime(0);
+                setRunning(false);
+            }}>Reset</button>
+        </div>
         </>
-    )
+    );
 }
+
 
 export default Timer;
